2a4ea3e88f3d8cb90edff62dd96c664dcbc4047f
Lopez <ialopez.contacto@gmail.com>
2025-01-20 11:25:29 +0100

    Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+In /src/lib/wagmi.js you need to put a walletconnect Project Name and ID if want to work with it
+
+Run locally -> npm run dev
--- src/lib/wagmi.js
+++ src/lib/wagmi.js
@@ -4,11 +4,11 @@ import { http } from 'wagmi';
 import { metaMask, walletConnect } from '@wagmi/connectors';
 
 const config = getDefaultConfig({
-  appName: 'McFarmers',
-  projectId: '91e4298f4ed8aa463e3565e8116a943f',
+  appName: '',
+  projectId: '',
   chains: [base],
   transports: {
-    [base.id]: http('https://rpc.ankr.com/base/9d117ee12554d5ad962f7c4be7e1aa2bf4c6376edb9f7d441934c9adaa82179a'),
+    [base.id]: http('https://rpc.ankr.com/base'),
   },
 });
 
