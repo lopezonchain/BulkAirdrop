33f9b1c444d2aa47683ebd801187a005fd26b401
Lopez <ialopez.contacto@gmail.com>
2025-03-07 00:43:43 +0100

    Update wagmi.js

--- src/lib/wagmi.js
+++ src/lib/wagmi.js
@@ -29,10 +29,10 @@ const etn = {
 const config = getDefaultConfig({
   appName: 'Buddy',
   projectId: '91e4298f4ed8aa463e3565e8116a943f',
-  chains: [base, etn],
+  chains: [etn, base],
   transports: {
-    [base.id]: http('https://rpc.ankr.com/base'),
     [etn.id]: http('https://rpc.ankr.com/electroneum'),
+    [base.id]: http('https://rpc.ankr.com/base'),
   },
 });
 
